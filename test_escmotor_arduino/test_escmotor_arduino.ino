01b4f8697b212269054402c16e4dd61179952822
Tim G <tdg24@duke.edu>
2023-04-08 18:56:11 -0400

    some arduino files

---
+++ test_escmotor_arduino/test_escmotor_arduino.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+//Pin 9: white (CTRL)
+//GND: Black
+//5V: Red
+
+//PSU @ max 1.03A (max0 & 11V
+
+Servo ESC;     // create servo object to control the ESC
+
+//int potValue;  // value from the analog pin
+
+void setup() {
+  // Attach the ESC on pin 9
+  ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+}
+
+void loop() {
+  //potValue = analogRead(A0);   // reads the value of the potentiometer (value between 0 and 1023)
+  //potValue = map(potValue, 0, 1023, 0, 180);   // scale it to use it with the servo library (value between 0 and 180)
+  ESC.write(100);    // Send the signal to the ESC
+}

ee1ffc0b251656c6f74f2911fe0e2be8e0320598
T Geissler <tdg24@duke.edu>
2023-04-16 11:41:50 -0400

    pot used

--- test_escmotor_arduino/test_escmotor_arduino.ino
+++ test_escmotor_arduino/test_escmotor_arduino.ino
@@ -25,8 +25,8 @@ void initESC() {
 }
 
 void loop() {
-  //potValue = analogRead(A0);   // reads the value of the potentiometer (value between 0 and 1023)
-  //potValue = map(potValue, 0, 1023, 1000, 2000);   // scale it to use it with the servo library (value between 0 and 180)
+  potValue = analogRead(A0);   // reads the value of the potentiometer (value between 0 and 1023)
+  potValue = map(potValue, 0, 1023, 1000, 2000);   // scale it to use it with the servo library (value between 0 and 180)
   // Attach the ESC on pin 9
-  ESC.writeMicroseconds(1500);
+  ESC.writeMicroseconds(potValue);
 }
